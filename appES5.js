3da20da568185748d9c4c256cdc062769ab2b1be
Fatih <fatihcihan666@gmail.com>
2023-03-02 18:59:17 +0300

    ui ctor created, addCourseToList and clearControl methods created

--- appES5.js
+++ appES5.js
@@ -1,7 +1,32 @@
+// Course ctor
 function Course(title, instructor, image) {
     this.title = title;
     this.instructor = instructor;
     this.image = image;
+}
+
+// UI ctor
+function UI() {
+
+}
+
+UI.prototype.addCourseToList = function (course) {
+    const list = document.getElementById('course-list');
+    var html = `
+        <tr>
+            <td><img src="img/${course.image}"/></td>
+            <td>${course.title}</td>
+            <td>${course.instructor}</td>
+            <td> <a href="#" class="btn btn-danger btn-sm"> Delete </a></td>
+        </tr>
+    `;
+    list.innerHTML += html;
+}
+
+UI.prototype.clearControls = function () {
+    const title = document.getElementById('title').value = "";
+    const instructor = document.getElementById('instructor').value = "";
+    const image = document.getElementById('image').value = "";
 }
 
 document.getElementById('new-course').addEventListener('submit', function (event) {
@@ -13,10 +38,14 @@ document.getElementById('new-course').addEventListener('submit', function (event
     // create course object
     const course = new Course(title, instructor, image)
 
-    console.log(course);
-    // save to database
+    // create UI
+    const ui = new UI();
 
-    // show on the ui
+    // add course to list 
+    ui.addCourseToList(course);
+
+    // clear controls
+    ui.clearControls();
 
     event.preventDefault();
 })
